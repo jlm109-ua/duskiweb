58eadc33f8d07b61152df47cbbc1cd44ac624f7e
Edu Ruiz <ruizcantosps@gmail.com>
2024-07-09 15:41:33 +0200

    fixed noob fail

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div class="flex justify-center align-top min-h-screen bg-black">
-      <h1 class="uppercase text-white">Duskies</h1>
+    <div className="flex justify-center align-top min-h-screen bg-black">
+      <h1 className="uppercase text-white">Duskies</h1>
     </div>
   );
 }
