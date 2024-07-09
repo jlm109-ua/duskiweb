3cafa353c0cefba93621556916a531db970bed62
Edu Ruiz <ruizcantosps@gmail.com>
2024-07-10 00:33:52 +0200

    added navbar component and imageSlider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import Navbar from "../components/Navbar";
+import ImageSlider from "../components/ImageSlider";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="flex justify-center align-top min-h-screen bg-black">
-      <h1 className="uppercase text-white">Duskies</h1>
+    <div className="flex-col justify-center align-top min-h-screen w-screen bg-black p-5">
+      <Navbar />
+      <div className="w-full flex justify-center items-center">
+        <ImageSlider />
+      </div>
+      <Image
+        src="/images/duskiesLogo.png"
+        alt="logo"
+        className="fixed bottom-3 right-3"
+        width={50}
+        height={50}
+      />
     </div>
   );
 }
