3ff6220bdd66255c856a27c34bea7bace0cc0c7a
jlm109-ua <jlm109@alu.ua.es>
2024-07-11 13:09:45 +0200

    LanguageSwitcher fixed

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -1,11 +1,23 @@
 "use client";
 
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import { useTranslations } from "next-intl";
+import { useEffect, useState } from "react";
 
 export default function LanguageSwitcher() {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  const t = useTranslations("LangSwitcher");
+
+  const [currentLang, setCurrentLang] = useState("en");
+
+  useEffect(() => {
+    const langInPath = pathname.split('/')[1];
+    if (langInPath && langInPath.match(/^[a-z]{2}$/)) {
+      setCurrentLang(langInPath);
+    }
+  }, [pathname]);
 
   const changeLanguage = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedLanguage = event.target.value;
@@ -18,9 +30,13 @@ export default function LanguageSwitcher() {
 
   return (
     <div>
-      <select className="bg-white text-black" onChange={changeLanguage}>
-        <option value="en">En</option>
-        <option value="es">Es</option>
+      <select
+        className="bg-white text-black"
+        value={currentLang}
+        onChange={changeLanguage}
+      >
+        <option value="en">{t('en')}</option>
+        <option value="es">{t('es')}</option>
       </select>
     </div>
   );
