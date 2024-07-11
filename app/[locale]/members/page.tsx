0ac246995e7b8eafb27118264455c93f934ac52f
jlm109-ua <jlm109@alu.ua.es>
2024-07-11 14:23:34 +0200

    Members page in progress

--- app/[locale]/members/page.tsx
+++ app/[locale]/members/page.tsx
@@ -1,9 +1,44 @@
 import Navbar from "@/components/Navbar";
+import { Box } from "@chakra-ui/react";
+import MemberSection from "@/components/MemberSection";
 
-export default function Members() {
+const members = [
+  /* FORMATO DE CADA MIEMBRO
+  {
+    image: '',
+    title: 'Member 1',
+    description: 'Description 1'
+  }
+  */
+  {
+    image:'/members/jimy.jpg',
+    title: 'Jimy',
+    description: 'Jimy is a very cool guy and loves destroying his Lexus.'
+  },
+  {
+    image:'/members/edu.jpg',
+    title: 'Edu',
+    description: 'Edu is a very cool guy and loves destroying his BMW as well.'
+  }
+]
+
+function MembersPage() {
   return (
     <div className="flex-col justify-center align-top min-h-screen w-screen bg-black p-5">
       <Navbar />
+      <Box
+        p={2}
+      >
+        {members.map((member, index) => (
+          <MemberSection
+            key={index}
+            member={member}
+            index={index}
+          />
+        ))}
+      </Box>
     </div>
   );
 }
+
+export default MembersPage
---
+++ components/MemberSection.tsx
@@ -0,0 +1,61 @@
+import { Box, Text, Image, Flex } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+import { chakra, shouldForwardProp } from '@chakra-ui/react';
+
+const MotionBox = chakra(motion.div, {
+    shouldForwardProp: prop => shouldForwardProp(prop) || prop === 'transition'
+})
+
+const MemberSection = ({ member, index }: { member: any, index: number }) => {
+    const { image, title, description } = member;
+    const isEven = index % 2 === 0;
+
+    return (
+        <MotionBox
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: "0.8", delay: `${index * 0.2}s` }}
+            mb={8}
+            p={4}
+            bg="white"
+            boxShadow="md"
+            borderRadius="md"
+        >
+            <Flex
+                direction={isEven ? 'row-reverse' : 'row'}
+                align="center"
+            >
+                <Box
+                    flex="1"
+                    mr={isEven ? 0 : 4}
+                    ml={isEven ? 4 : 0}
+                >
+                    <Image
+                        src={image}
+                        alt={title}
+                        borderRadius="md"
+                    />
+                </Box>
+                <Box
+                    flex="2"
+                >
+                    <Text
+                        fontSize="xl"
+                        fontWeight="bold"
+                        color="gray.800"
+                    >
+                        {title}
+                    </Text>
+                    <Text
+                        fontSize="md"
+                        color="gray.600"
+                    >
+                        {description}
+                    </Text>
+                </Box>
+            </Flex>
+        </MotionBox>
+    )
+}
+
+export default MemberSection
