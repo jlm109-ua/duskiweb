7b59ef9e1ed42bc63bc92f899d868203c34fc275
jimy <jimy@devuntu.devuntu>
2024-07-16 20:26:47 +0200

    Pagination done

--- app/[locale]/members/page.tsx
+++ app/[locale]/members/page.tsx
@@ -1,25 +1,19 @@
-import { Box } from "@chakra-ui/react";
+"use client";
+
+import { useState } from "react";
+import { Box, Button, Flex } from "@chakra-ui/react";
 import MemberSection from "@/components/MemberSection";
 import { Title } from "@/components/Title";
 import { useTranslations } from "next-intl";
 
-/**
- * Obtiene los miembros dinámicamente de las traducciones. Tiene que llamarse useGetMembers por usar una hook en su body.
- * @param t - La función de traducción.
- * @returns Lista de miembros con su información.
- */
 const useGetMembers = () => {
   const t = useTranslations();
   const members = t.raw("Members");
   const ignoreKeys = ["title", "meetTheMember", "specs", "media", "specsTitle", "modsTitle"];
 
-  // DEBUG
-  // console.log("DEBUG: ", members);
-
   const memberKeys = Object.keys(members).filter(
     (key) => !ignoreKeys.includes(key)
   );
-
 
   return memberKeys.map((key) => ({
     title: key,
@@ -30,12 +24,9 @@ const useGetMembers = () => {
   }));
 };
 
-function MembersPage() {
+/* function MembersPage() {
   const t = useTranslations("Members");
   const members = useGetMembers();
-
-  // DEBUG
-  // console.log("DEBUG - MEMBERS: ", members);
 
   return (
     <>
@@ -49,6 +40,55 @@ function MembersPage() {
       </Box>
     </>
   );
+} */
+
+function MembersPage() {
+  const t = useTranslations("Members");
+  const tP = useTranslations("Pagination");
+  const members = useGetMembers();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentMembers = members.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalPages = Math.ceil(members.length / itemsPerPage);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  return (
+    <>
+      <Box className="mt-10">
+        <Title>{t("title")}</Title>
+      </Box>
+      <Box p={2}>
+        {currentMembers.map((member, index) => (
+          <MemberSection key={index} member={member} index={index} />
+        ))}
+      </Box>
+      <Flex justifyContent="center" mt={4}>
+        <Button onClick={handlePreviousPage} disabled={currentPage === 1} mr={2}>
+          {tP("previous")}
+        </Button>
+        <Button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          {tP("next")}
+        </Button>
+      </Flex>
+    </>
+  );
 }
+
 
 export default MembersPage;
